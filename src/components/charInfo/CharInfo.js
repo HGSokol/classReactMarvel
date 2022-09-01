bb8651f56272abc35e87d363f2e0e73ab6a495c4
Соколов Глеб <g.sokolov@21vek.local>
2022-09-01 13:20:36 +0300

    repert other

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types'
 
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner.js';
@@ -34,11 +35,6 @@ class CharInfo extends Component{
             this.updateChar()
         }
     }
-
-    // componentDidCatch(err, inf){
-    //     console.log(err)
-    //     console.log(inf)
-    // }
 
     updateChar = () => {
         const {charId} = this.props
@@ -137,6 +133,10 @@ const View = ({char}) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
 }
 
 
